e6ca1e6c213d0708d645edfa6f70fb2ce0ac56c2
Lawrence Radburn <lawrence@radnet.ca>
2024-05-10 20:51:10 -0400

    feat: add song functions

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ let userData = {
 const renderSongs = (array) => {
   const songsHTML = array.map((song) => {
     return `<li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button onclick="playSong(${song.id})" class="playlist-song-info">
         <span class="playlist-song-title">${song.title}</span>
         <span class="playlist-song-artist">${song.artist}</span>
         <span class="playlist-song-duration">${song.duration}</span>
@@ -125,4 +125,23 @@ const playSong = (id) => {
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
   }
+  else {
+    audio.currentTime = userData?.songCurrentTime;
+  } 
+    userData.currentSong = song;
+    playButton.classList.add("playing");
+    audio.play();
 }
+
+const pauseSong = () => {
+
+};
+
+playButton.addEventListener("click", () => {
+  if (!userData.currentSong) {
+    playSong(userData?.songs[0].id);
+  }
+  else {
+    playSong(userData?.currentSong.id);
+  }
+});
