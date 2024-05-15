607a834da63a53360e290e0a156c76d264b790d5
Lawrence Radburn <lawrence@radnet.ca>
2024-05-15 16:36:41 -0400

    feat: accessible play text

--- script.js
+++ script.js
@@ -103,6 +103,11 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
+};
+
 const getCurrentSongIndex = () => {
   return userData?.songs.indexOf(userData?.currentSong);
 };
@@ -137,6 +142,7 @@ const playSong = (id) => {
     highlightCurrentSong();
     audio.play();
     setPlayerDisplay();
+    setPlayButtonAccessibleText();
 };
 
 // Store current time of the song
